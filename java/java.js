54b8b13a69bdca7f7f9787e2c7c0b930c7914e78
Omarardila07 <ardilaomar848@gmail.com>
2023-09-23 11:12:38 -0500

    calculadora3

--- java/java.js
+++ java/java.js
@@ -33,23 +33,3 @@ $(document).ready(function () {
         $(".calculator").toggleClass("dark-mode");
         $(".number").toggleClass("dark-mode");
     });
-
-    // Cambio de colores RGB
-    $("#red, #green, #blue").on("input", function () {
-        const red = $("#red").val();
-        const green = $("#green").val();
-        const blue = $("#blue").val();
-
-        $("body").css("background-color", `rgb(${red}, ${green}, ${blue})`);
-        $(".calculator").css("background-color", `rgb(${255 - red}, ${255 - green}, ${255 - blue})`);
-        $(".calculator").css("color", `rgb(${red}, ${green}, ${blue})`);
-        $(".header h1").css("color", `rgb(${255 - red}, ${255 - green}, ${255 - blue})`);
-        $(".screen").css("background-color", `rgb(${255 - red}, ${255 - green}, ${255 - blue})`);
-        $(".screen").css("color", `rgb(${red}, ${green}, ${blue})`);
-        $(".buttons button").css("background-color", `rgb(${red}, ${green}, ${blue})`);
-        $(".buttons button").css("color", `rgb(${255 - red}, ${255 - green}, ${255 - blue})`);
-        $(".buttons button:hover").css("background-color", `rgb(${red - 20}, ${green - 20}, ${blue - 20})`);
-        $(".buttons button:hover").css("color", `rgb(${255 - red + 20}, ${255 - green + 20}, ${255 - blue + 20})`);
-        $(".slider.round:before").css("background-color", `rgb(${255 - red}, ${255 - green}, ${255 - blue})`);
-    });
-});
